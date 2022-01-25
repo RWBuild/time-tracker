f05c15ee92aa950bda76dfcaaf19b1fcb210f9c9
manirabona walker <55847682+manirabona-programer@users.noreply.github.com>
2022-01-25 14:55:35 +0200

    modify web routes: add get routes as /read in routes

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/read', function () {
-    return view('ready to be readed');
-});
