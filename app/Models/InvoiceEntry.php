2b07c323d1fcbacbee31bc470798b878268b327a
sabatohura <sabato@hura.one>
2022-01-25 21:22:39 +0200

    added all models

---
+++ app/Models/InvoiceEntry.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InvoiceEntry extends Model
+{
+    use HasFactory;
+}
