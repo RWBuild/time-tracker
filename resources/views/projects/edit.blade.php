b7fae856453d0f64cf74f4eb66efbd60d324167b
manirabona-programer <hseal419@gmail.com>
2022-01-26 17:26:20 +0200

    update edit view: rename projects to project

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>edit {{$projects->name}}</title>
+    <title>edit {{$project->name}}</title>
 </head>
 <body>
     <div class="project">
-        <p><strong>Name => </strong>{{$projects->name}}</p>
-        <p><strong>Description => </strong>{{$projects->description}}</p>
-        <p><strong>Budget => </strong>{{$projects->budget}}</p>
+        <p><strong>Name => </strong>{{$project->name}}</p>
+        <p><strong>Description => </strong>{{$project->description}}</p>
+        <p><strong>Budget => </strong>{{$project->budget}}</p>
     </div>
 </body>
 </html>
