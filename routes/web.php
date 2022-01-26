32c65b62c85b081b28ae769f7763415fb2d3c5bc
habaumugisha1 <habajeunes2@gmail.com>
2022-01-26 14:18:48 +0200

    client;tests; test creat clien,test edit client, test index list all clients, test show client

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::post('/clients', 'App\Http\Controllers\ClientController@store');
+Route::resource('/clients', 'App\Http\Controllers\ClientController');
---
+++ tests/Feature/ClientTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Client;
+
+class ClientTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+     use RefreshDatabase;
+
+     public function test_user_can_create_a_client()
+     {
+         $this->withoutExceptionHandling();
+
+         $response = $this->post('/clients',[
+             'name' => 'ABC Company', 
+             'code' => 'ABCCO',
+             'address' => 'Kimironko',
+             'phone' => '628-724-4139',
+          ]);
+  
+          $response->assertStatus(200);
+  
+         $this->assertTrue(Client::all()->count() == 1);
+     }
+
+    public function test_user_can_update_a_client()
+    {
+        $client = Client::factory()->create(['name' => 'ABC Company']);
+
+        $this->assertDatabaseHas('clients', ['name' => 'ABC Company']);
+
+        $response = $this->put('/clients/'.$client->id, [
+            'name' => 'ABC Company Updated',
+            'code' => $client->code
+        ]);
+
+        $this->assertDatabaseHas('clients', ['name' => 'ABC Company Updated']);
+    }
+
+    public function test_user_can_view_a_client()
+   {
+       $this->withoutExceptionHandling();
+       $client = Client::factory()->create();
+       $this->assertTrue(Client::all()->count() == 1);
+
+       $response = $this->get('/clients/'.$client->id);
+
+       $response->assertStatus(200);
+       $response->assertSee($client->name);
+   }
+
+   public function test_user_can_delete_a_client()
+   {
+       $client = Client::factory()->create();
+       $this->assertTrue(Client::all()->count() == 1);
+       $this->delete('/clients/'.$client->id);
+       $this->assertTrue(Client::all()->count() == 0 );
+   }
+}
+
