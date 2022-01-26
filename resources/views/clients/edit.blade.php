d72493c68c1dd09e148f336196eeda9d98fc8e12
GuevaraMarius <ilizasanicole1@gmail.com>
2022-01-26 12:58:06 +0200

    add client model controller routes factory tests

---
+++ resources/views/clients/edit.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+    <h2>Edit</h2>
+    <p>Name: {{$client->name}}</p>
+    <p>code: {{$client->code}}</p>
+    <p>phone: {{$client->phone}}</p>
+    <p>address: {{$client->address}}</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/clients','App\Http\Controllers\ClientController');
+// Route::post('/clients', 'App\Http\Controllers\ClientController@store');
+// Route::put('/clients/{client}', 'App\Http\Controllers\ClientController@update');
