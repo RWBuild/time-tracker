e160ce640e31fef18d9146f9bfd67b35a3e140cd
hugues0 <ntwari.hugues@gmail.com>
2022-01-26 15:02:56 +0200

    fixed model

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -56,8 +56,7 @@ class ClientController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  Client  $client     * @return \Illuminate\Http\Response
      */
     public function edit(Client $client)
     {
