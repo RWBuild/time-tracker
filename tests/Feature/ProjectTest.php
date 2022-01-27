a62952e96b78614feca964b591fd078ed704f5b5
Sanicole35 <ilizasanicole1@gmail.com>
2022-01-27 06:48:13 -0800

    Roles and ProjectTest

--- tests/Feature/ProjectTest.php
+++ tests/Feature/ProjectTest.php
@@ -6,16 +6,24 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Project;
+use App\Models\User;
 
 class ProjectTest extends TestCase
 {
   use RefreshDatabase;
 
+
+  public function setUp(): void{
+    parent::setUp();
+    $this->user = User::factory()->create();
+  }
+
+
   public function test_user_can_create_a_project()
   {
     $this->withoutExceptionHandling();
 
-    $response = $this->post('/projects',[
+    $response = $this->actingAs($this->user)->post('/projects',[
       'client_id' => 1,
       'name' => 'ABC Project',
       'description' => 'This is a description',
@@ -27,6 +35,24 @@ class ProjectTest extends TestCase
     $this->assertTrue(Project::all()->count() == 1);
   }
 
+
+  public function test_guest_can_not_create_a_project()
+  {
+    $this->withoutExceptionHandling();
+
+    $response = $this->actingAs($this->user)->post('/projects',[
+      'client_id' => 1,
+      'name' => 'ABC Project',
+      'description' => 'This is a description',
+      'budget' => 10000.14,
+    ]);
+
+    $response->assertStatus(200);
+
+    $this->assertTrue(Project::all()->count() == 1);
+  }
+
+
   public function test_user_can_update_a_project()
   {
     //$this->withoutExceptionHandling();
@@ -37,7 +63,26 @@ class ProjectTest extends TestCase
 
     $this->assertDatabaseHas('projects',['name' => 'ABC Project']);
 
-    $response = $this->put('/projects/'.$project->id, [
+    $response = $this->actingAs($this->user)->put('/projects/'.$project->id, [
+      'client_id' => $project->client_id,
+      'name' => 'ABC Project Updated',
+    ]);
+
+    $this->assertDatabaseHas('projects',['name' => 'ABC Project Updated']);
+    
+  }
+
+  public function test_guest_can_not_update_a_project()
+  {
+    //$this->withoutExceptionHandling();
+
+    $project = Project::factory()->forClient()->create([
+      'name' => 'ABC Project'
+    ]);
+
+    $this->assertDatabaseHas('projects',['name' => 'ABC Project']);
+
+    $response = $this->actingAs($this->user)->put('/projects/'.$project->id, [
       'client_id' => $project->client_id,
       'name' => 'ABC Project Updated',
     ]);
@@ -48,12 +93,12 @@ class ProjectTest extends TestCase
 
   public function test_user_can_see_a_project()
   {
-    $this->withoutExceptionHandling();
+    //$this->withoutExceptionHandling();
 
     $project = Project::factory()->forClient()->create();
     $this->assertTrue(Project::all()->count() == 1);
 
-    $response = $this->get('/projects/'.$project->id);
+    $response = $this->actingAs($this->user)->get('/projects/'.$project->id);
     $response->assertStatus(200);
     $response->assertSee($project->name);
   }
@@ -63,8 +108,20 @@ class ProjectTest extends TestCase
     $project = Project::factory()->forClient()->create();
     $this->assertTrue(Project::all()->count() == 1);
 
-    $response = $this->delete('/projects/'.$project->id);
+    $response = $this->actingAs($this->user)->delete('/projects/'.$project->id);
     $this->assertTrue(Project::all()->count() == 0);
 
   }
+
+
+  public function test_guest_can_not_delete_a_project()
+  {
+    $project = Project::factory()->forClient()->create();
+    $this->assertTrue(Project::all()->count() == 1);
+
+    $response = $this->actingAs($this->user)->delete('/projects/'.$project->id);
+    $this->assertTrue(Project::all()->count() == 0);
+
+  }
+
 }
