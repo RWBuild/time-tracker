5d380972607e5ddb5c8ccd7414bde506b2b64662
Dan Merfeld <dan@theorythree.com>
2022-01-27 18:43:52 +0200

    Updates project tests to include guest check on crud methods

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,8 @@ Route::get('/dashboard', function () {
 
 require __DIR__.'/auth.php';
 
-Route::resource('/projects','App\Http\Controllers\ProjectController');
-
 // LOGGED IN USER GROUP
 Route::group(['middleware' => 'auth'], function() {
   Route::resource('/clients','App\Http\Controllers\ClientController');
+  Route::resource('/projects','App\Http\Controllers\ProjectController');
 });
