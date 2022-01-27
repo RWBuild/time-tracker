c65f16d6793eb4987337b1ce76795174e94b8bb9
Dan Merfeld <dan@theorythree.com>
2022-01-27 09:46:28 +0200

    adds project tests, model, views, controllers, and updates routes

---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Project: {{ $project->name }}</title>
+</head>
+<body>
+  
+  <div>
+    <h2>Edit {{ $project->name }}</h2>
+    <p>Name: {{ $project->name }}</p>
+    <p>Client: {{ $project->client_id }}</p>
+    <p>{{ $project->budget }}</p>
+    <p>{{ $project->description }}</p>
+  </div>
+
+</body>
+</html>
---
+++ tests/Feature/ProjectTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Project;
+
+class ProjectTest extends TestCase
+{
+  use RefreshDatabase;
+
+  public function test_user_can_create_a_project()
+  {
+    $this->withoutExceptionHandling();
+
+    $response = $this->post('/projects',[
+      'client_id' => 1,
+      'name' => 'ABC Project',
+      'description' => 'This is a description',
+      'budget' => 10000.14,
+    ]);
+
+    $response->assertStatus(200);
+
+    $this->assertTrue(Project::all()->count() == 1);
+  }
+
+  public function test_user_can_update_a_project()
+  {
+    //$this->withoutExceptionHandling();
+
+    $project = Project::factory()->create([
+      'name' => 'ABC Project'
+    ]);
+
+    $this->assertDatabaseHas('projects',['name' => 'ABC Project']);
+
+    $response = $this->put('/projects/'.$project->id, [
+      'client_id' => $project->client_id,
+      'name' => 'ABC Project Updated',
+      
+    ]);
+
+    $this->assertDatabaseHas('projects',['name' => 'ABC Project Updated']);
+    
+  }
+
+  public function test_user_can_see_a_project()
+  {
+    $this->withoutExceptionHandling();
+
+    $project = Project::factory()->create();
+    $this->assertTrue(Project::all()->count() == 1);
+
+    $response = $this->get('/projects/'.$project->id);
+    $response->assertStatus(200);
+    $response->assertSee($project->name);
+  }
+
+  public function test_user_can_delete_a_project()
+  {
+    $project = Project::factory()->create();
+    $this->assertTrue(Project::all()->count() == 1);
+
+    $response = $this->delete('/projects/'.$project->id);
+    $this->assertTrue(Project::all()->count() == 0);
+
+  }
+}
