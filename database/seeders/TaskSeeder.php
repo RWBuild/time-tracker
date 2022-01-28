36a1380ca48a6fdb2aeff24facc354608a2d121b
iradukunda28celine <iradukunda.ireneceline28@gmail.com>
2022-01-28 21:02:33 +0200

    added time-entry model,task model, created fcrms

---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Task;
+use Illuminate\Support\Facades\DB;
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    
+        DB::table('Tasks')->truncate();
+        Task::create(['id'=> 1, 'name'=> 'Administration']);
+        Task::create(['id'=> 2, 'name'=> 'Backend Development']);
+        Task::create(['id'=> 3, 'name'=> 'Frontend Development']);
+        Task::create(['id'=> 4, 'name'=> 'Project Management']);
+        Task::create(['id'=> 5, 'name'=>  'Testing']);
+        
+
+
+    }
+}
