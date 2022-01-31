a8ccb4124526cc8f104cc82c844b72f08bb68039
Dan Merfeld <dan@theorythree.com>
2022-01-31 09:48:12 +0200

    stubs views for time entry

---
+++ resources/views/time-entries/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+  @forelse ($timeEntries as $timeEntry)
+      <p>{{ $timeEntry->project->name }} {{ $timeEntry->duration }}</p>
+  @empty
+      <p>No time entries for this date.</p>
+  @endforelse
+  
+</body>
+</html>
---
+++ resources/views/time-entries/show.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Time Entry: {{ $timeEntry->date }}</title>
+</head>
+<body>
+  
+<p>Project: {{ $timeEntry->project->name }}</p>
+<p>Task: {{ $timeEntry->task->name }}</p>
+
+</div>
+
+</body>
+</html>
