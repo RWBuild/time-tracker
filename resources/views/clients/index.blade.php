5a3b43a5d2e2ca8a287ed613576d6d1b0bd0a149
laetitia123 <uwizelaetitia@gmail.com>
2022-02-01 16:52:36 +0200

    clients table

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -8,11 +8,59 @@
 </head>
 <body>
 
-  @forelse ($clients as $client)
-      <p>{{ $client->name }} {{ $client->code }}</p>
+  @foreach ($clients as $client)
+  
+      {{-- <p>{{ $client->name }} {{ $client->code }}</p> --}}
+      <h2>Clients</h2>
+        <table>
+            <tr>
+                <th>No</th>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Email</th>
+                <th>Action</th>
+
+            <tr>
+                <tbody>
+                    @foreach ($clients as $client)
+                        <tr>
+                            <td>
+                                {{ $client->id }}
+                            </td>
+                            <td>
+                                {{ $client->first_name }}
+                            </td>
+
+                            <td>
+                                {{ $client->last_name }}
+                            </td>
+
+                            <td>
+                                {{ $client->email }}
+                            </td>
+                            <td class="action">
+                                <a href="/clients/{{ $client->id }}" class="action-view">view</a>
+
+                                <form method="POST" action="">
+                                    @csrf
+                                    @method('PATCH')
+                                    <button type="submit" class="action-delete">Edit</button>
+                                </form>
+                            </td>
+                        </tr>
+                </tbody>
+
+
+
+
+
+                @endforeach
+        </table>
+
   @empty
       <p>No clients in the database.</p>
-  @endforelse
+
+  {{-- @endforelse --}}
   
 </body>
 </html>
