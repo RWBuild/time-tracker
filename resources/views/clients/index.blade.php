bd286d97b52b975dcc927cf8df18982d679a95e2
laetitia123 <uwizelaetitia@gmail.com>
2022-02-01 18:12:22 +0200

    create style.css file

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -5,13 +5,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
 
   {{-- @foreach ($clients as $client) --}}
   
       {{-- <p>{{ $client->name }} {{ $client->code }}</p> --}}
-      <h2>Clients</h2>
+      <div class="content-children">
+      <h2> List of Clients</h2>
         <table>
             <tr>
                 <th>No</th>
@@ -43,8 +45,8 @@
 
                                 <form method="POST" action="">
                                     @csrf
-                                    @method('PATCH')
-                                    <button type="submit" class="action-delete">Edit</button>
+                                    @method('PUT')
+                                    <button type="submit" class="">Edit</button>
                                 </form>
                             </td>
                         </tr>
@@ -60,6 +62,7 @@
 
 
         </table>
+      </div>
 
   {{-- @empty
       <p>No clients in the database.</p> --}}
