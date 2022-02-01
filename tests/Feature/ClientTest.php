3faa77ce5d8e708c1cf587d0547e8769532192e5
GuevaraMarius <irabizimguevara@gmail.com>
2022-02-01 10:52:14 +0200

    add client  policy & update client controller,request and test

--- tests/Feature/ClientTest.php
+++ tests/Feature/ClientTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Client;
+use App\Models\Role;
 use App\Models\User;
 
 class ClientTest extends TestCase
@@ -15,12 +16,26 @@ class ClientTest extends TestCase
   public function setUp(): void
   {
     parent::setUp();
+    $this->artisan('db:seed --class=RoleSeeder');
     $this->user = User::factory()->create();
+    $this->user->roles()->attach(Role::IS_USER);
+    $this->owner = User::factory()->create();
+    $this->owner->roles()->attach(Role::IS_OWNER);
   }
 
-  public function test_user_can_create_a_client()
+  public function test_user_can_not_create_a_client()
   {
     $response = $this->actingAs($this->user)->post('/clients',[
+      'name' => 'ABC Company',
+      'code' => 'ABCCO'
+    ]);
+
+    $response->assertStatus(403);
+    $this->assertTrue(Client::all()->count() == 0);
+  }
+  public function test_owner_can_create_a_client()
+  {
+    $response = $this->actingAs($this->owner)->post('/clients',[
       'name' => 'ABC Company',
       'code' => 'ABCCO'
     ]);
@@ -40,7 +55,7 @@ class ClientTest extends TestCase
     $this->assertTrue(Client::all()->count() == 0);
   }
 
-  public function test_user_can_update_a_client()
+  public function test_user_can_not_update_a_client()
   {
     $client = Client::factory()->create([
       'name' => 'ABC Company'
@@ -49,6 +64,21 @@ class ClientTest extends TestCase
     $this->assertDatabaseHas('clients',['name' => 'ABC Company']);
 
     $response = $this->actingAs($this->user)->put('/clients/'.$client->id, [
+      'name' => 'ABC Company Updated',
+      'code' => $client->code,
+    ]);
+    $response->assertStatus(403);
+    $this->assertDatabaseHas('clients',['name' => 'ABC Company']);
+  }
+  public function test_owner_can_update_a_client()
+  {
+    $client = Client::factory()->create([
+      'name' => 'ABC Company'
+    ]);
+
+    $this->assertDatabaseHas('clients',['name' => 'ABC Company']);
+
+    $response = $this->actingAs($this->owner)->put('/clients/'.$client->id, [
       'name' => 'ABC Company Updated',
       'code' => $client->code,
     ]);
@@ -84,14 +114,25 @@ class ClientTest extends TestCase
     $response->assertSee($client->name);
   }
 
-  public function test_user_can_delete_a_client()
+  public function test_user_can_not_delete_a_client()
   {
     $client = Client::factory()->create();
     $this->assertTrue(Client::all()->count() == 1);
 
     $response = $this->actingAs($this->user)->delete('/clients/'.$client->id);
+    $this->assertTrue(Client::all()->count() == 1);
+    $response->assertStatus(403);
+  }
+
+  public function test_user_owner_delete_a_client()
+  {
+    $client = Client::factory()->create();
+    $this->assertTrue(Client::all()->count() == 1);
+
+    $response = $this->actingAs($this->owner)->delete('/clients/'.$client->id);
     $this->assertTrue(Client::all()->count() == 0);
   }
+
 
   public function test_guest_can_not_delete_a_client()
   {
