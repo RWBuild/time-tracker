0fbc7f76592b796819a0207ac9472a6af695e3ab
Robert Ngabo <robertngabo2002@gmail.com>
2022-02-01 16:43:44 +0200

    blue frontend inititalization -- adding default icons lib

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
         <title>@yield('page-title')</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body>
