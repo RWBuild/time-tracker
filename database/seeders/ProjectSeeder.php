fd645e2d690f79dc57e358ac8465bedd6de22b59
Dev Account <devaccount@Dacys-Macbook.local>
2022-02-02 10:54:55 +0200

    run seeds and migration

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -17,7 +17,7 @@ class ProjectSeeder extends Seeder
         \DB::table('projects')->truncate();
 
       Project::create(['id' => 1, "client_id" => 1, "name" => 'E-commerce', "budget" =>2000.00]);
-      Project::create(['id' => 2, "client_id" => 3, "name" => 'CRM', "budget" =>1500.00]);
+      Project::create(['id' => 2, "client_id" => 1, "name" => 'CRM', "budget" =>1500.00]);
       Project::create(['id' => 3, "client_id" => 4, "name" => 'Hotel Management App', "budget" =>3000.00]);
     }
 }
