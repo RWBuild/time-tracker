f8bbf1436a51f586f5197d390d241cf5760fe294
laetitia123 <uwizelaetitia@gmail.com>
2022-02-02 11:57:02 +0200

    add project form

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,14 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Create Project</title>
-</head>
-<body>
-  
-  This is where a form to create a project would go...
+@extends('layouts.main')
 
-</body>
-</html>
+
+
+@section('content')
+    <div class="form_page">
+        <form action="{{ route('clients.store') }}" method="POST">
+            <h1>Add Project</h1>
+            @csrf
+            <input type="text" placeholder="Fullname" name="name" />
+            @error('name')
+                <span class="message-color">
+                    {{ $message }}
+                </span>
+            @enderror
+            <input type="text" placeholder="Budget" name="Budget" />
+            @error('code')
+                <span class="message-color">
+                    {{ $message }}
+                </span>
+            @enderror
+            <textarea placeholder="Decription" name="decription"></textarea>
+            @error('description')
+                <span class="message-color">
+                    {{ $message }}
+                </span>
+            @enderror
+            {{-- <select name="person_id" id="person_id">
+              @foreach ($clients as $person)
+                  <option value="{{ $client->id }}">
+
+                      {{ $client->name }} 
+                  </option>
+              @endforeach
+          </select> --}}
+
+            <button type="submit"> Submit</button>
+        </form>
+    </div>
+@endsection
