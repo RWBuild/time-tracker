b96b39f5b64866dd1ba1b2e71956315f06457c9c
laetitia123 <uwizelaetitia@gmail.com>
2022-02-02 14:40:51 +0200

    create project form

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -4,17 +4,25 @@
 
 @section('content')
     <div class="form_page">
-        <form action="{{ route('clients.store') }}" method="POST">
+        <form action="{{ route('projects.store') }}" method="POST">
             <h1>Add Project</h1>
             @csrf
-            <input type="text" placeholder="Fullname" name="name" />
+            <select name="client_id">
+                @foreach ($clients as $client)
+                    <option value="{{ $client->id }}">
+
+                        {{ $client->name }}
+                    </option>
+                @endforeach
+            </select>
+            <input type="text" placeholder="Projectname" name="name" />
             @error('name')
                 <span class="message-color">
                     {{ $message }}
                 </span>
             @enderror
-            <input type="text" placeholder="Budget" name="Budget" />
-            @error('code')
+            <input type="text" placeholder="Budget" name="budget" />
+            @error('budget')
                 <span class="message-color">
                     {{ $message }}
                 </span>
@@ -25,14 +33,7 @@
                     {{ $message }}
                 </span>
             @enderror
-            {{-- <select name="person_id" id="person_id">
-              @foreach ($clients as $person)
-                  <option value="{{ $client->id }}">
 
-                      {{ $client->name }} 
-                  </option>
-              @endforeach
-          </select> --}}
 
             <button type="submit"> Submit</button>
         </form>
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,13 +1,27 @@
 @extends('layouts.main')
 @section('content')
+@if (session('success'))
+@component('components.card')
+    @slot('content')
+        <p class="text-green-600">{{ session('success') }}</p>
+    @endslot
+@endcomponent
+@endif
 
-  @forelse ($projects as $project)
-      <p>{{ $project->name }} {{ $project->budget }}</p>
+    @forelse ($clients as $client)
+        <p>{{ $client->name }} </p>
+        @forelse($client->projects as $project)
+            <p>{{ $project->name }}</p>
+            <a href="projects/create">Create Project</a>
 
-  @empty
-      <p>No projects in the database.</p>
-  @endforelse
-  <a href="projects/create">Create Project</a>
+
+        @empty
+            <p>No projects in the database.</p>
+        @endforelse
+        @empty
+        <p>No clients in the database.</p>
+
+    @endforelse
+    <a href="projects/create">Create Project</a>
 
 @endsection
-  
