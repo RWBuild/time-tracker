c0bc337b680142e9209266f78d9d7499dfb25c78
unknown <dusenaberawilliam07@gmail.com>
2022-02-02 18:08:34 +0200

    project views

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,12 +2,20 @@
 
 
 @section('content')
+    <h2 class="text-center text-3xl pt-5"> List of Projects</h2>
+    @if (session('success'))
+        @component('components.card')
+            @slot('content')
+                <p class="text-green-600">{{ session('success') }}</p>
+            @endslot
+        @endcomponent
+    @endif
     <div class="flex flex-wrap w-11/12 mx-auto">
         @forelse ($clients as $client)
             @component('components.card')
                 @slot('content')
                     <h2 class="text-xl">Client</h2>
-                    
+
                     <hr>
                     <p>{{ $client->name }}</p>
                     <h2 class="text-xl mt-5">Projects</h2>
@@ -19,7 +27,11 @@
                                     <p>{{ $project->name }} {{ $project->budget }}</p>
                                 </td>
                                 <td>
-                                    <a class="ml-5 text-blue-600" href="/projects/{{ $project->id }}">View</a>
+                                    <a class="text-blue-600" href="/projects/{{ $project->id }}">View</a>
+                                    @if (Auth::User()->isAdmin() || Auth::User()->isOwner())
+                                        <a class="text-blue-600" href="/projects/{{ $project->id }}/edit">Edit</a>
+
+                                    @endif
                                 </td>
                             </tr>
                         @empty
