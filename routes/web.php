45006c668565bce138cb33a1a7a0abc722c22895
prince <nshutiprinceberyl@gmail.com>
2022-02-03 12:25:31 +0200

    adds a searchByDate function in timeEntryController

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 // ADMIN ROLE GROUP
-Route::group(['middleware' => 'is_admin'], function() {
-  Route::get('/dashboard', function () { return view('dashboard'); })->name('dashboard');
+Route::group(['middleware' => 'is_admin'], function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
 
-Route::group(['middleware' => 'is_owner'], function() {
-  Route::get('/owner', function () { return 'you are an owner'; })->name('owner');
+Route::group(['middleware' => 'is_owner'], function () {
+    Route::get('/owner', function () {
+        return 'you are an owner';
+    })->name('owner');
 });
 
 // LOGGED IN USER GROUP
-Route::group(['middleware' => 'auth'], function() {  
-  Route::resource('/clients','App\Http\Controllers\ClientController');
-  Route::resource('/projects','App\Http\Controllers\ProjectController');
-  Route::resource('/time-entries','App\Http\Controllers\TimeEntryController');
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('/clients', 'App\Http\Controllers\ClientController');
+    Route::resource('/projects', 'App\Http\Controllers\ProjectController');
+    Route::get('/time-entries/searchByDate/{date}', 'App\Http\Controllers\TimeEntryController@searchByDate');
+    Route::resource('/time-entries', 'App\Http\Controllers\TimeEntryController');
 });
